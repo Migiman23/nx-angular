c96cc24db4d8a1e73699690005c47256f9b112f0
Migiman23 <manuellopezp1@hotmail.com>
2021-08-03 15:17:38 -0500

    first commit :v

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Todo } from '@myorg/data';
 
 @Component({
   selector: 'myorg-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'todos';
+export class AppComponent implements OnInit {
+  // title = 'todos';
+  todos: Todo[] = [];
+  
+  constructor(private http: HttpClient) {}
+  
+  ngOnInit(): void {
+    this.fetch()
+  }
+
+  fetch() {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (
+      this.todos = t
+    ))
+  }
+
+  addTodo() {
+    this.http.post('/api/addTodo',{}).subscribe(() => {
+      this.fetch()
+    })
+  }
 }
